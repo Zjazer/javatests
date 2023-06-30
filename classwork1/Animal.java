39a659b825e9a8f542f16ac6d34aeef3e613829a
Zjazer <115031085+Zjazer@users.noreply.github.com>
2023-06-30 04:46:47 -0700

    Add files via upload

---
+++ classwork1/Animal.java
@@ -0,0 +1,19 @@
+
+public class Animal {
+        public static void animal(string[] args)
+        private String name;
+        private String color;
+        private int age;
+        private String country;
+
+        public void setAttributes(String name, String surname, int age, String country) {
+                this.name = name;
+                this.color = color;
+                this.age = age;
+                this.country = country;
+        }
+
+        public String getDetails() {
+                return "name: " + name + " color: " + color + "age: " + age + " country: " + country;
+        }
+
---
+++ classwork1/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        animal animal1 = new animal);
+        animal.ributes("black", "cat", 35, "Indonesia");
+        System.out.println(animal1.getDetails());
+
+        animal2 = new animal;
+        person2.setAttributes("black", "Dog", 30, "USA");
+        System.out.println(person2.animal());
+    }
+}
+
+    }
+}
